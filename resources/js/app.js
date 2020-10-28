a5a9472c0618de1530f21e36a586482c2299eb93
rashadulalam <rashadul91@gmail.com>
2020-10-28 07:09:05 +0600

    React scafolding

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
-require('./bootstrap');
+import React from 'react';
+import ReactDom from 'react-dom';
+import Main from './Main';
+
+ReactDom.render(
+    <React.StrictMode>
+        <Main />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
